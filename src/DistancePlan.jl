8cdd0ac8bdc5a266ae52093bca62be50bf450389
K.R. Zentner <krzentner@gmail.com>
2019-10-19 12:15:49 -0700

    Files generated by PkgTemplates

---
+++ src/DistancePlan.jl
@@ -0,0 +1,5 @@
+module DistancePlan
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DistancePlan
+using Test
+
+@testset "DistancePlan.jl" begin
+    # Write your own tests here.
+end
