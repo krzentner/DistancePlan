a9bc0b6a9248cae28de71054dcef5fa7e3a971a7
K.R. Zentner <krzentner@gmail.com>
2019-10-22 11:02:25 -0700

    Implement basic obstacles.

--- src/DistancePlan.jl
+++ src/DistancePlan.jl
@@ -1,5 +1,42 @@
 module DistancePlan
 
-greet() = print("Hello World!")
+norm(v)::Float64 = sqrt(sum(v .^ 2))
+
+distance(center, obstacle::Vector{Float64})::Float64 = norm(center .- obstacle)
+
+struct Box
+    lows::Vector{Float64}
+    highs::Vector{Float64}
+end
+
+function distance(center, obstacle::Box)::Float64
+    diffs = [(obstacle.lows .- center) (center .- obstacle.highs)]
+    max_diffs = maximum(diffs, dims=2)
+    return norm(max_diffs)
+end
+
+struct Sphere
+    center::Vector{Float64}
+    radius::Float64
+end
+
+function distance(center, obstacle::Sphere)::Float64
+    return norm(center .- obstacle.center) - obstacle.radius
+end
+
+function distance(center, obstacle::Vector)::Float64
+    return minimum([distance(center, obs) for obs=obstacle])
+end
+
+struct SphereTree
+    centers::Array{Float64}
+    squared_radii:Array{Float64}
+    down::Vector{Vector{UInt}}
+    up::Vector{UInt}
+end
+
+function nearest_sphere(tree::SphereTree, point::Vector{Float64})::UInt
+    return argmin(sum((point .- centers) .^ 2, dims=2) - tree.squared_radii)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using DistancePlan
 using Test
 
 @testset "DistancePlan.jl" begin
+    @test DistancePlan.distance([0., 0.], [1., 1.]) == sqrt(2)
+    @test DistancePlan.distance([0., 0.],
+                                DistancePlan.Box([1., 1.], [2., 2.])) == sqrt(2)
+    @test DistancePlan.distance([0., 0.],
+                                DistancePlan.Sphere([1., 1.], 0.5)) == sqrt(2) - 0.5
+    @test DistancePlan.distance([0., 0.], [[0., 0.], [1., 1.]]) == 0.
     # Write your own tests here.
 end
