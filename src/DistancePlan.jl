720fee1b0c4bb13c389a51deb53fab7c3095c070
K.R. Zentner <krzentner@gmail.com>
2019-11-01 21:23:28 -0700

    Add more detailed type annotations

--- src/DistancePlan.jl
+++ src/DistancePlan.jl
@@ -1,56 +1,62 @@
 module DistancePlan
 
-norm(v)::Float64 = sqrt(sum(v .^ 2))
-
-distance(center, obstacle::Vector{Float64})::Float64 = norm(center .- obstacle)
-
-struct Box
-    lows::Vector{Float64}
-    highs::Vector{Float64}
+function norm(v::Array{T, N})::T where {T <: AbstractFloat, N}
+  return sqrt(sum(v .^ 2))
 end
 
-function distance(center, obstacle::Box)::Float64
+function distance(center::Array{T, N}, obstacle::Array{T, N})::T where {T <: AbstractFloat, N}
+  return norm(center .- obstacle)
+end
+
+abstract type Volume end
+
+struct Box{T <: Number, N} <: Volume
+    lows::Array{T, N}
+    highs::Array{T, N}
+end
+
+function distance(center::Array{T, N}, obstacle::Box{T, N})::T where {T <: AbstractFloat, N}
     diffs = [(obstacle.lows .- center) (center .- obstacle.highs)]
     max_diffs = maximum(diffs, dims=2)
     return norm(max_diffs)
 end
 
-struct Sphere
-    center::Vector{Float64}
-    radius::Float64
+struct Sphere{T <: Number, N} <: Volume
+    center::Array{T, N}
+    radius::T
 end
 
-function distance(center, obstacle::Sphere)::Float64
+function distance(center::Array{T, N}, obstacle::Sphere{T, N})::T where {T <: AbstractFloat, N}
     return norm(center .- obstacle.center) - obstacle.radius
 end
 
-function distance(center, obstacle::Vector)::Float64
+function distance(center::Array{T, N}, obstacle::AbstractVector)::T where {T <: AbstractFloat, N}
     return minimum([distance(center, obs) for obs=obstacle])
 end
 
-struct SphereTree
-    centers::Vector{Vector{Float64}}
-    squared_radii::Vector{Vector{Float64}}
+struct SphereTree{T <: Number, N}
+    centers::Vector{Array{T, N}}
+    squared_radii::Vector{Array{T, N}}
     children::Vector{Vector{UInt}}
     parent::Vector{UInt}
 end
 
-function nearest_sphere(tree::SphereTree, point::Vector{Float64})::UInt
+function nearest_sphere(tree::SphereTree{T, N}, point::Array{T, N})::UInt where {T <: AbstractFloat, N}
     return argmin(sum((point .- centers) .^ 2, dims=2) - tree.squared_radii)
 end
 
-function project(point::Vector{Float64}, sphere::Sphere)::Vector{Float64}
+function project(point::Array{T, N}, sphere::Sphere{T, N})::Array{T, N} where {T <: AbstractFloat, N}
   v = point .- sphere.center
   v /= norm(v)
   return v .* sphere.radius
 end
 
-function sample(bounds::Box)::Vector{Float64}
+function sample(bounds::Box{T, N})::Array{T, N} where {T <: AbstractFloat, N}
   r = rand(length(bounds.lows))
   return (bounds.highs .- bounds.lows) .* r .+ bounds.low
 end
 
-function push_sphere!(tree::SphereTree, parent::UInt, sphere::Sphere)
+function push_sphere!(tree::SphereTree{T, N}, parent::UInt, sphere::Sphere{T, N}) where {T <: AbstractFloat, N}
   child = length(tree.parent)
   push!(tree.centers, sphere.center)
   push!(tree.squared_radii, sphere.radius ^ 2)
@@ -58,11 +64,11 @@ function push_sphere!(tree::SphereTree, parent::UInt, sphere::Sphere)
   push!(tree.children[parent], child)
 end
 
-function get_sphere(tree::SphereTree, sphere::UInt)::Sphere
+function get_sphere(tree::SphereTree{T, N}, sphere::UInt)::Sphere{T, N} where {T <: AbstractFloat, N}
   return Sphere(tree.centers[sphere], sqrt(tree.squared_radii[sphere]))
 end
 
-function evt_step!(tree::SphereTree, bounds::Box, obstacles::Vector)
+function evt_step!(tree::SphereTree{T, N}, bounds::Box{T, N}, obstacles::Vector) where {T <: AbstractFloat, N}
   x = sample(bounds)
   x_near = nearest_sphere(tree, point)
   x_new = project(x, get_sphere(tree, x_near))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+import Pkg
+Pkg.activate(".")
 using DistancePlan
 using Test
 
